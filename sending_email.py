81905fadd9212e7313f4d7ec900175b130fe7910
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 14:20:44 +0300

    Updated the code so that it gets to use environment variables

--- sending_email.py
+++ sending_email.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,6 +7,6 @@ print('Composing Email....')
 
 SERVER = 'smtp.gmail.com'
 PORT = 587
-FROM = ''
-TO = ''
-PASS = ''
+FROM = os.getenv("FROM_EMAIL_ADDRESS")
+TO = os.getenv("TO_EMAIL_ADDRESS")
+PASS = os.getenv("FROM_EMAIL_PASSWORD")
