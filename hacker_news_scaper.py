7fcbd5f57ea77a25c77deb0a2b7e9b466803c873
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 12:25:27 +0300

    Rearranged the import statements

--- hacker_news_scaper.py
+++ hacker_news_scaper.py
@@ -1,11 +1,13 @@
-# HTTP requests
-import requests
 
-# WebScraping
-from bs4 import BeautifulSoup
 # EmailBody
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+# WebScraping
+from bs4 import BeautifulSoup
+
+# HTTP requests
+import requests
 # System Date and Time Manipulation
 import datetime
 
