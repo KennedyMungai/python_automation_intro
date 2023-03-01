3725ebcc1a78bc0a32f9ad1b639dd08272d1b798
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 15:40:33 +0300

    Added an if else block for exception handling in the code

--- ted_talk_downloader.py
+++ ted_talk_downloader.py
@@ -5,3 +5,10 @@ from bs4 import BeautifulSoup
 import re
 
 import sys
+
+
+# Exception Handling
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+else:
+    sys.exit('Error: Please enter the TED Talk URL')
