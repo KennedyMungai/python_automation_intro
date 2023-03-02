73efcf06fa30cb6240188e5e4aa7d18e70c09ab2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-02 08:27:49 +0300

    Added an extra backslash

--- ted_talk_downloader.py
+++ ted_talk_downloader.py
@@ -23,7 +23,7 @@ for val in soup.findAll("script"):
     if (re.search("talkPage.init", str(val))) is not None:
         result = str(val)
 
-result_mp4 = re.search("(?P<url>https?://[^\s]+)(mp4)", result).group("url")
+result_mp4 = re.search("(?P<url>https?://[^\\s]+)(mp4)", result).group("url")
 mp4_url = result_mp4.split('"')[0]
 
 print('Downloading video from ...'+mp4_url)
